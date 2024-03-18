bb76bb0e756ea1c09fdd4c77f59e7d5050eaf70b
Squarehead2 <118778307+Squarehead2@users.noreply.github.com>
2024-03-17 21:16:07 -0600

    Update instockdesigns.js

--- src/InStockDesigns/instockdesigns.js
+++ src/InStockDesigns/instockdesigns.js
@@ -1,42 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { listAll, ref, getDownloadURL } from 'firebase/storage';
-import { storage } from '../_utils/firebase';
+import React, { useState, useEffect } from "react";
+import { listAll, ref, getDownloadURL } from "firebase/storage";
+import { storage } from "../_utils/firebase";
 import "./InStockDesigns.css";
 
 export default function InStockDesigns() {
-    const [imageList, setImageList] = useState([]);
+  const [imageList, setImageList] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            // List and fetch images from 'patterns/type1/' location
-            const patternsRef = ref(storage, 'patterns/type1/');
-            const patternsSnapshot = await listAll(patternsRef);
-            const patternsUrls = await Promise.all(patternsSnapshot.items.map(async (item) => {
-                const url = await getDownloadURL(item);
-                return url;
-            }));
+  useEffect(() => {
+    const fetchData = async () => {
+      // List and fetch images from 'patterns/type1/' location
+      const patternsRef = ref(storage, "patterns/type1/");
+      const patternsSnapshot = await listAll(patternsRef);
+      const patternsUrls = await Promise.all(
+        patternsSnapshot.items.map(async (item) => {
+          const url = await getDownloadURL(item);
+          return url;
+        })
+      );
 
-            // List and fetch images from 'patterns/type2/' location
-            const type2Ref = ref(storage, 'patterns/type2/');
-            const type2Snapshot = await listAll(type2Ref);
-            const type2Urls = await Promise.all(type2Snapshot.items.map(async (item) => {
-                const url = await getDownloadURL(item);
-                return url;
-            }));
+      // List and fetch images from 'patterns/type2/' location
+      const type2Ref = ref(storage, "patterns/type2/");
+      const type2Snapshot = await listAll(type2Ref);
+      const type2Urls = await Promise.all(
+        type2Snapshot.items.map(async (item) => {
+          const url = await getDownloadURL(item);
+          return url;
+        })
+      );
 
-            // Concatenate both lists of image URLs
-            setImageList([...patternsUrls, ...type2Urls]);
-        };
+      // Concatenate both lists of image URLs
+      setImageList([...patternsUrls, ...type2Urls]);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            {imageList.map((url, index) => (
-                
-                <><img key={index} src={url} alt={`Image ${index}`} /><p></p></>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-5 bg-white">
+        {imageList.map((url, index) => (
+          <>
+            <div className="card w-96 h-[32rem] shadow-xl ">
+              <figure className="shadow-xl">
+                <img
+                  className="w-96 h-96"
+                  key={index}
+                  src={url}
+                  alt={`Image ${index}`}
+                />
+              </figure>
+              <div className="card-body bg-primary-50">
+                <h2 className="card-title">Name</h2>
+                <p className="text-black text-sm">
+                  Description of the product. This is a placeholder description
+                </p>
+                <div className="card-actions justify-end bg-primary-50 rounded-xl">
+                  <div className="stats shadow bg-white w-full h-30">
+                    <div className="stat bg-white w-full">
+                      <div className="stat-title bg-white w-full">Price</div>
+                      <div className="stat-value bg-white w-full">
+                        $XX.XX / cm<sup>2</sup>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="badge badge-outline">Type of Pattern</div>
+                </div>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
