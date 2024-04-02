8120e1a6b015423987fba17ec6b7d8ef248b2428
MilesGDKing <mdolphyking35@gmail.com>
2024-04-01 19:01:17 -0600

    finished carousel
    continued css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,17 @@
-import * as React from "react";
+import React from "react";
 import "./Home.css";
-import { listAll, ref, getDownloadURL } from 'firebase/storage';
-import { storage } from '../_utils/firebase';
+import carousel1 from "./carousel/carousel1.jpg";
+import carousel2 from "./carousel/carousel2.jpg";
+import carousel3 from "./carousel/carousel3.jpg";
+import carousel4 from "./carousel/carousel4.jpg";
+
+
 function Home() {
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   return (
     <div className="App">
       <header className="App-header">
-        <p className="default">
+        <div className="rounded-box">
+          <h6>Welcome to Foster Hydrodipping</h6>
           Transforming your gear from mundane to magnificent has never been
           easier with Foster Hydro Dipping! Dive into a world of endless
           possibilities as we specialize in hydrographic printing, giving your
@@ -19,42 +23,22 @@ function Home() {
           creativity and let us turn your ordinary possessions into
           extraordinary works of art. Experience the magic of hydro dipping
           today and elevate your belongings to new levels of visual appeal!
-        </p>
+        </div>
       </header>
       <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    {/* Replace the image with your desired image */}
-    <img src="YOUR_IMAGE_URL_1" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    {/* Replace the image with your desired image */}
-    <img src="YOUR_IMAGE_URL_2" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    {/* Replace the image with your desired image */}
-    <img src="YOUR_IMAGE_URL_3" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    {/* Replace the image with your desired image */}
-    <img src="YOUR_IMAGE_URL_4" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div>
+        <div id="item1" className="carousel-item w-full">
+          <img src={carousel1} className="w-full" alt="Slide 1" />
+        </div> 
+        <div id="item2" className="carousel-item w-full">
+          <img src={carousel2} className="w-full" alt="Slide 2" />
+        </div> 
+        <div id="item3" className="carousel-item w-full">
+          <img src={carousel3} className="w-full" alt="Slide 3" />
+        </div> 
+        <div id="item4" className="carousel-item w-full">
+          <img src={carousel4} className="w-full" alt="Slide 4" />
+        </div>
+      </div> 
     </div>
   );
 }
