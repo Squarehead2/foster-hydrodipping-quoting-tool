6ce65bc09438bc810311d8e0f29ee5e13a7f766e
Squarehead2 <118778307+Squarehead2@users.noreply.github.com>
2024-04-16 14:54:31 -0600

    Revert "Removed story book tests from main branch"
    This reverts commit bda2f536680cadfce1eb635a6c7de6a6c39e5a8f.

---
+++ src/stories/objectAdditions/objectAddition2/ObjectAddition2.stories.js
@@ -0,0 +1,41 @@
+import objectAddition from "../../../pages/Calculator/Mathematics/objectAddition";
+import ObjectAddition from "../../../pages/Calculator/Mathematics/objectAddition";
+import object from "../../../pages/Calculator/Objects/object";
+import { use, useState } from "react";
+import { useEffect } from "react";
+
+export default {
+  component: ObjectAddition,
+};
+
+const Template = (args) => {
+  const [objects, setObjects] = useState([
+    object("Rectangle", 100),
+    object("Circle", 100),
+  ]);
+
+  useEffect(() => {
+    setObjects((prevObjects) => [
+      ...prevObjects,
+      object("Triangle", -100),
+      object("Square", 100),
+    ]);
+  }, []);
+
+  const [objectSum, setObjectSum] = useState(0);
+
+  useEffect(() => {
+    setObjectSum(objectAddition(objects));
+  }, [objects]);
+
+  return (
+    <>
+      <h1>Object Addition</h1>
+      <p>Objects: {JSON.stringify(objects)}</p>
+      <br />
+      objectSum: {objectSum}
+    </>
+  );
+};
+
+export const Primary = Template.bind({});
